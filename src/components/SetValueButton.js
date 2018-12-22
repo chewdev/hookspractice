821bcd1296108d1ece6d6ca9525708afede50c9d
chewdev <itsbewald@gmail.com>
2018-12-21 22:43:23 -0800

    Update button text

--- src/components/SetValueButton.js
+++ src/components/SetValueButton.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function SetValueButton({ input1, setSetValue }) {
-  return <button onClick={() => setSetValue(input1)}>set value</button>;
+  return <button onClick={() => setSetValue(input1)}>Set Value</button>;
 }
